d31b6658c86294d3440894ccc8f46d35a8267216
estherlaurette <esther.laurette@gmail.com>
2016-03-21 10:03:23 +0100

    Acceuil

---
+++ Accueil.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>MyBoost - Le Site Web</title>
+        <link rel="stylesheet" type="text/css" href="AccueilCSS.css">
+    </head>
+    <body>
+        <div id="bloc_page">
+            <header>
+                <div id="titre_principal">
+                    <div class="logo">
+                            <img src="C:\Users\Esther\Documents\MyBoost\Logo4.png" alt="Logo de site web MyBoost"/>
+                            <h1>Un sport, un clic, une communauté!</h1>
+                        </ul>
+                    </div>
+                    <div class="connexion">
+                        <a href="/sign-up">Sign up</a>
+                    </div>
+                </div>
+                <nav>
+                    <ul>
+                        <li class="nav-item"><a href="#">Accueil</a></li>
+                        <li class="nav-item"><a href="#">Mon Compte</a></li>
+                        <li class="nav-item"><a href="#">Communauté</a>
+                           <!--<ul class="sub-nav">
+                                <li class="sub-nav-item"><a href="#">Sports</a></li>
+                                <li class="sub-nav-item"><a href="#">Annuaire</a></li>
+                                <li class="sub-nav-item"><a href="#">Groupes</a></li>
+                            </ul>
+                        </li>-->
+                        <li class="nav-item"><a href="#">Forum</a></li>
+                        <li class="nav-item"><a href="#">À Propos</a></li>
+                    </ul>
+                </nav>
+                <div class="search">
+                    <input type="text" name="name" placeholder="rechercher">
+                </div>
+            </header>
+            <footer>
+                <div class="effet">
+                    <a href="https://www.facebook.com/MyBoost-1136877939682445/?fref=ts"><img src="C:\Users\Esther\Documents\MyBoost\footer\fb.png" alt="facebook"></a>
+                    &nbsp;
+                    <a href="https://twitter.com/myb00st"><img src="C:\Users\Esther\Documents\MyBoost\footer\twitter.jpg" alt="twitter"></a>
+                    &nbsp;
+                    <a href="#"><img src="C:\Users\Esther\Documents\MyBoost\footer\insta.jpg" alt="insta"></a>
+                    &nbsp;
+                    MyBoost &copy; 2016 - Tous droits r&eacute;serv&eacute;s
+                </div>
+            </footer>
+        </div>
+    </body>
+</html>

5c3ce8670ce37702027f9934b6b24cef8cef8725
estherlaurette <esther.laurette@gmail.com>
2016-03-22 09:46:29 +0100

    logo accueil

--- Accueil.php
+++ Accueil.php
@@ -10,7 +10,7 @@
             <header>
                 <div id="titre_principal">
                     <div class="logo">
-                            <img src="C:\Users\Esther\Documents\MyBoost\Logo4.png" alt="Logo de site web MyBoost"/>
+                            <img src="C:\wamp\www\MyBoost\Images\Logo4.png" alt="Logo de site web MyBoost"/>
                             <h1>Un sport, un clic, une communauté!</h1>
                         </ul>
                     </div>

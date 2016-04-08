6dc8e40bbf869e8a66379fd8154421759e8afd2e
jblasalle <jeanbenoitlasalle@gmail.com>
2016-04-08 11:47:48 +0200

    MVC Sports

--- sports.php
+++ sports.php
@@ -4,52 +4,52 @@ include('header.php');
 
 ?>
 
-<html> 
+<html>
 <head>
 <meta charset = "utf-8">
 <link rel="stylesheet" href="stylesheet.css" >
-<title>Activités sportives</title>	
+<title>ActivitÃ©s sportives</title>
 </head>
 <body>
-	
+
 	<div id="bloc_page">
 
-            <div class="top"><p>Activités</p>
+            <div class="top"><p>ActivitÃ©s</p>
                 </div>
 
 	<div class="sports-aquatiques"><h2 class="categorie-sports">Sports aquatiques</h2>
 
-		<a href="sportChoisi.php?sport=<?php echo('Natation');?>"><img class="imgsport" src="Images/natation.jpg"></a>
-		<a href="sportChoisi.php?sport=<?php echo('Wakeboard');?>"><img class="imgsport" src="Images/wakeboard.jpg"></a>
+		<a href="sportChoisi.php?sport=<?php echo('Natation');?>"><img class="imgsport" src="Images/clipart_Natation.png"></a>
+		<a href="sportChoisi.php?sport=<?php echo('Wakeboard');?>"><img class="imgsport" src="Images/clipart_Wakeboard.png"></a>
 
-	</div>	
+	</div>
 
 	<div class="sports-plein-air"><h2 class="categorie-sports">Sports en plein air</h2>
 
-		<a href="sportChoisi.php?sport=<?php echo('Tennis');?>"><img class="imgsport" src="Images/clipart_tennis.png"></a>
-		<a href="sportChoisi.php?sport=<?php echo('Fotball');?>"><img class="imgsport" src="Images/clipart_football.png"></a>
-		<a href="sportChoisi.php?sport=<?php echo('Roller');?>"><img class="imgsport" src="Images/roller-skater.jpg"></a>
-		<a href="sportChoisi.php?sport=<?php echo('Escalade');?>"><img class="imgsport" src="Images/Escalade.jpg"></a>
+		<a href="sportChoisi.php?sport=<?php echo('Tennis');?>"><img class="imgsport" src="Images/clipart_Tennis.png"></a>
+		<a href="sportChoisi.php?sport=<?php echo('Fotball');?>"><img class="imgsport" src="Images/clipart_Football.png"></a>
+		<a href="sportChoisi.php?sport=<?php echo('Roller');?>"><img class="imgsport" src="Images/clipart_Roller.png"></a>
+		<a href="sportChoisi.php?sport=<?php echo('Escalade');?>"><img class="imgsport" src="Images/clipart_Escalade.png"></a>
 
-	</div>	
+	</div>
 
 	<div class="sports-en-salle"><h2 class="categorie-sports">Sports en salle</h2>
 
-		<a href="sportChoisi.php?sport=<?php echo('Handball');?>"><img class="imgsport" src="Images/handball.jpg"></a>
-		<a href="sportChoisi.php?sport=<?php echo('Boxe');?>"><img class="imgsport" src="Images/clipart_boxe.png"></a>
-		<a href="sportChoisi.php?sport=<?php echo('Basket');?>"><img class="imgsport" src="Images/clipart_basket.png"></a>
-		<a href="sportChoisi.php?sport=<?php echo('Tennis');?>"><img class="imgsport" src="Images/clipart_tennis.png"></a>
-		<a href="sportChoisi.php?sport=<?php echo('Basket');?>"><img class="imgsport" src="Images/clipart_football.png"></a>
-		<a href="sportChoisi.php?sport=<?php echo('Escalade en salle');?>"><img class="imgsport" src="Images/Escalade-salle.jpg"></a>
-	</div>	
+		<a href="sportChoisi.php?sport=<?php echo('Handball');?>"><img class="imgsport" src="Images/clipart_Handball.png"></a>
+		<a href="sportChoisi.php?sport=<?php echo('Boxe');?>"><img class="imgsport" src="Images/clipart_Boxe.png"></a>
+		<a href="sportChoisi.php?sport=<?php echo('Basket');?>"><img class="imgsport" src="Images/clipart_Basket.png"></a>
+		<a href="sportChoisi.php?sport=<?php echo('Tennis');?>"><img class="imgsport" src="Images/clipart_Tennis.png"></a>
+		<a href="sportChoisi.php?sport=<?php echo('Basket');?>"><img class="imgsport" src="Images/clipart_Football.png"></a>
+		<a href="sportChoisi.php?sport=<?php echo('Escalade en salle');?>"><img class="imgsport" src="Images/clipart_Escalade-en-salle.png"></a>
+	</div>
 
 	<div class="sports-co"><h2 class="categorie-sports">Sports collectifs</h2>
 		<a href=""><img class="imgsport" src="Images/clipart_football.png"></a>
 		<a href=""><img class="imgsport" src="Images/clipart_basket.png"></a>
-		<a href=""><img class="imgsport" src="Images/handball.jpg"></a>
-		<a href=""><img class="imgsport" src="Images/Volleyball.jpeg"></a>
+		<a href=""><img class="imgsport" src="Images/clipart_Handball.png"></a>
+		<a href=""><img class="imgsport" src="Images/clipart_Volleyball.png"></a>
 	</div>
-	
+
 <?php
 
 include('footer.php');
@@ -58,4 +58,4 @@ include('footer.php');
 
         </div>
 </body>
-</html>	
+</html>

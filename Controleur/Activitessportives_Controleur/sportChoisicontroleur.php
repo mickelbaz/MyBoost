9531c5184341dbd85b0e64389c48e371da6495d7
jblasalle <jeanbenoitlasalle@gmail.com>
2016-04-14 11:48:00 +0200

    chgmt_activitessportives

---
+++ Controleur/Activitessportives_Controleur/sportChoisicontroleur.php
@@ -0,0 +1,11 @@
+<?php
+
+include('header.php');
+
+
+ echo($_GET['sport']); 
+
+ echo($_GET{'description'});
+
+include('footer.php');
+?>
---
+++ Modele/Activitessportives_Modele/Activitessportivesmodele.php
@@ -0,0 +1,15 @@
+<?php
+<a href="sportChoisi.php?sport=<?php echo('Natation');?>"</a>
+<a href="sportChoisi.php?sport=<?php echo('Wakeboard');?>"</a>
+<a href="sportChoisi.php?sport=<?php echo('Tennis');?>"</a>
+<a href="sportChoisi.php?sport=<?php echo('Football');?>"</a>
+<a href="sportChoisi.php?sport=<?php echo('Roller');?>"</a>
+<a href="sportChoisi.php?sport=<?php echo('Escalade');?>"</a>
+<a href="sportChoisi.php?sport=<?php echo('Handball');?>"</a>
+<a href="sportChoisi.php?sport=<?php echo('Boxe');?>"</a>
+<a href="sportChoisi.php?sport=<?php echo('Basket');?>"</a>
+<a href="sportChoisi.php?sport=<?php echo('Tennis');?>"</a>
+<a href="sportChoisi.php?sport=<?php echo('Basket');?>"</a>
+<a href="sportChoisi.php?sport=<?php echo('Escalade en salle');?>"</a>
+<a href=""><img class="imgsport" src="Images/clipart_football.png"></a>
+?>
